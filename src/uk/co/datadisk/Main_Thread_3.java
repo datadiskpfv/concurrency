bd9d9f0179737506cd23dd5cb9b9ca95fc38325c
pvalle <paul.valle@orbian.com>
2018-09-17 13:48:47 +0100

    Playing around with Threads and synchronization, looking at reentrantLock

--- src/uk/co/datadisk/Main_Thread_3.java
+++ src/uk/co/datadisk/Main_Thread_3.java
@@ -7,27 +7,45 @@ public class Main_Thread_3 {
     // Classic Producer and Consumer Thread demo
     public static void main(String[] args) {
 
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+
     }
 }
 
 class Message {
     private String message;
-    private boolean empty = true;
+    private boolean empty = true;  // shared variable between threads
 
     public synchronized String read() {
         while(empty) {
-
+            try {
+                System.out.println("==== read is waiting");
+                wait();
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
         empty = true;
+        System.out.println("=== read is notifying");
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while(!empty) {
-
+            try {
+                System.out.println("==== write is waiting");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         empty = false;
         this.message = message;
+        System.out.println("=== write is notifying");
+        notifyAll();
     }
 }
 
@@ -55,10 +73,33 @@ class Writer implements Runnable {
             try {
                 Thread.sleep(random.nextInt(2000));
             } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        message.write("Finished");
+    }
+}
 
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()){
+
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e){
+                e.printStackTrace();
             }
         }
 
-        message.write("Finished");
     }
 }
---
+++ src/uk/co/datadisk/Main_Thread_4.java
@@ -0,0 +1,96 @@
+package uk.co.datadisk;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static uk.co.datadisk.Main_Thread_4.EOF;
+
+public class Main_Thread_4 {
+
+    public static final String EOF = "EOF";
+
+    public static void main(String[] args) {
+        List<String> buffer = new ArrayList<>();
+        ReentrantLock bufferLock = new ReentrantLock();
+
+        MyProducer myProducer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
+        MyConsumer myConsumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+        MyConsumer myConsumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
+
+        new Thread(myProducer).start();
+        new Thread(myConsumer1).start();
+        new Thread(myConsumer2).start();
+    }
+}
+
+class MyProducer implements Runnable {
+
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
+
+        for (String num : nums) {
+            try {
+                System.out.println(color + "Adding..." + num);
+                bufferLock.lock();
+                buffer.add(num);
+                bufferLock.unlock();
+
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                System.out.println("Producer was interrupted");
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting...");
+        bufferLock.lock();
+        buffer.add("EOF");
+        bufferLock.unlock();
+    }
+}
+
+class MyConsumer implements Runnable {
+
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+
+            bufferLock.lock();
+            if (buffer.isEmpty()) {
+                bufferLock.unlock();
+                continue;
+            }
+
+            if (buffer.get(0).equals(EOF)) {
+                System.out.println(color + "Exiting");
+                bufferLock.unlock();
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+            bufferLock.unlock();
+        }
+    }
+}
