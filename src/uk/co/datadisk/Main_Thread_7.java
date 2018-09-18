b479bce24346ff4f1373c653d33a98a2fbaff676
pvalle <paul.valle@orbian.com>
2018-09-18 09:15:57 +0100

    looking at thread deadlocks

--- src/uk/co/datadisk/Main_Thread_7.java
+++ src/uk/co/datadisk/Main_Thread_7.java
@@ -24,7 +24,26 @@ public class Main_Thread_7 {
                 }
                 System.out.println("Thread 1: released lock2");
             }
-            System.out.println("Thread1: realesed lock1. Exiting....");
+            System.out.println("Thread1: released lock1. Exiting....");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            synchronized (lock2){
+                System.out.println("Thread 2: has lock2");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e){}
+
+                System.out.println("Thread1: waiting for lock1");
+                synchronized (lock1){
+                    System.out.println("Thread2: has lock1 and lock2");
+                }
+                System.out.println("Thread 2: released lock1");
+            }
+            System.out.println("Thread1: released lock1. Exiting....");
         }
     }
 }
